6a621aa5107a76f5920aa2d23fcd676a6cd81c87
margaiwangara <margaiwangara@gmail.com>
2021-12-29 22:29:10 +0300

    client-side nextjs initialization and setup

---
+++ client/src/theme.ts
@@ -0,0 +1,21 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#588658',
+    secondary: '#ebeee7',
+    black: '#000000',
+    grey: '#f5f5f5',
+    white: '#ffffff',
+    danger: '#dc3545',
+    success: '#28a745',
+    warning: '#ffc107',
+    info: '#17a2b8',
+  },
+  breakpoints: {
+    sm: '576px',
+    md: '768px',
+  },
+};
+
+export default theme;
